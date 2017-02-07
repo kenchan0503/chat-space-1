5ec5c476d6a71961f3e1c9cb40e805c53b722df9
shimizushinichi <q_shinichii@yahoo.co.jp>
2017-02-07 14:20:51 +0900

    delete-useless-file, modify-DB-setting

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170207043522) do
+ActiveRecord::Schema.define(version: 20170207034318) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "nickname",                            null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -25,8 +26,8 @@ ActiveRecord::Schema.define(version: 20170207043522) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["nickname"], name: "index_users_on_nickname", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
