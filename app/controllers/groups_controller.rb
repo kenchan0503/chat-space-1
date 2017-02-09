4aa472382f0b6778cc8606157c4fd2789f779286
shimizushinichi <q_shinichii@yahoo.co.jp>
2017-02-09 16:26:41 +0900

    make-group-create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,22 @@
 class GroupsController < ApplicationController
+  def new
+    @group = Group.new
+    @users = User.all
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: "グループが作成されました。"
+    else
+      render :new
+    end
+    # TODO:messagesのルーティングと各グループの表示を設定して、作成したグループに飛ぶようにする。
+    # TODO:グループ名一覧をindexのleft-contentに表示させる。
+  end
+
+  private
+    def group_params
+      params.require(:group).permit(:name, user_ids:[])
+    end
 end
