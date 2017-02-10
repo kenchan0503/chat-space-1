bafaef5ec310a6a96e573e7f55bc1a0efefee86e
shimizushinichi <q_shinichii@yahoo.co.jp>
2017-02-10 12:48:19 +0900

    メイン画面の各要素表示を実装
    タイムゾーンの変更も実装。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,12 @@
 class MessagesController < ApplicationController
   def index
+    #サイドバー表示用
     @user = User.find(current_user.id)
     @groups = @user.groups
+
+    #メイン画面表示用
+    @group = Group.find(params[:group_id])
+    @messages = @group.messages
+    @users = @group.users
   end
 end
