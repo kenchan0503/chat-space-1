a6bc4eb01498a751e1a82f169332a7ee0530dca7
shimizushinichi <q_shinichii@yahoo.co.jp>
2017-02-11 12:18:54 +0900

    メッセージ送信機能の実装
    空欄でも送信できちゃってる

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,30 @@
 class MessagesController < ApplicationController
+
   def index
-    #サイドバー表示用
+  #サイドバー表示用
     @groups = current_user.groups
-    #メイン画面表示用
+
+  #メイン画面表示用
     @group = Group.find(params[:group_id])
     @messages = @group.messages
     @users = @group.users
+
+  #メッセージ送信用
+    @message = Message.new
   end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(params[:group_id])
+    else
+      render :index
+    end
+  end
+
+  private
+    def message_params
+      params.require(:message).permit(:body).merge(group_id: params[:group_id], user_id: current_user.id)
+    end
+
 end
