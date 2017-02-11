bcc3d6cdcbbf7c2f95a518d6860c226ba46a560d
shimizushinichi <q_shinichii@yahoo.co.jp>
2017-02-11 14:58:35 +0900

    redirect_toをrenderに変更
    測ったら処理が倍早かった

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,29 +1,36 @@
 class MessagesController < ApplicationController
 
   def index
-  #サイドバー表示用
-    @groups = current_user.groups
-
-  #メイン画面表示用
-    @group = Group.find(params[:group_id])
-    @messages = @group.messages
-    @users = @group.users
-
-  #メッセージ送信用
-    @message = Message.new
+    set_variables
   end
 
   def create
     @message = Message.new(message_params)
     if @message.save
-      redirect_to group_messages_path(params[:group_id])
+      set_variables
+      render :index
     else
-      redirect_to group_messages_path(params[:group_id]), alert: "何かメッセージを入力してください。"
+      set_variables
+      flash.now[:alert] = "何かメッセージを入力してください。"
+      render :index
     end
   end
 
   private
     def message_params
       params.require(:message).permit(:body).merge(group_id: params[:group_id], user_id: current_user.id)
+    end
+
+    def set_variables
+    #サイドバー表示用
+    @groups = current_user.groups
+
+    #メイン画面表示用
+    @group = Group.find(params[:group_id])
+    @messages = @group.messages
+    @users = @group.users
+
+    #メッセージ送信用
+    @message = Message.new
     end
 end
