98b2b4c3f7ad1f5e8d55136d054482eef7e043da
shimizushinichi <q_shinichii@yahoo.co.jp>
2017-02-14 13:16:33 +0900

    一つにまとめる

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -7,24 +7,17 @@ describe Message do
       message = build(:message)
       expect(message).to be_valid
     end
-  end
-end
 
-describe Message do
-  describe '#create' do
     it "is valid with a body, even if without a image" do
       message = build(:message, image:"")
       expect(message).to be_valid
     end
-  end
-end
 
-describe Message do
-  describe '#create' do
     it "is invalid without body" do
       message = build(:message, body:"")
       message.valid?
       expect(message.errors[:body]).to include("translation missing: ja.activerecord.errors.models.message.attributes.body.blank")
     end
+
   end
 end
