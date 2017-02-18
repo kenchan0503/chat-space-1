3784fb906c3133717d98abd145c5d5c60c63dd6d
shimizushinichi <q_shinichii@yahoo.co.jp>
2017-02-18 12:46:16 +0900

    スクロールのメソッド化+軽微な修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,14 +3,19 @@ $(function() {
     //データを受け取って、ulの中身を作って返す。
     var html = '<li class = "message__contents">'
     + '<p class = "message__contents__sender" >' + message.nickname
-    + '<p class = "message__contents__time" >' + message.created_at + " +0900"
+    + '<p class = "message__contents__time" >' + message.created_at
     + '<p class = "message__contents__content" >' + message.body;
-    console.log(html) //実装完了時に消します。
+    console.log(html) //画像投稿機能実装完了後、消します。
     return html;
   }
 
+  function scroll_chatview(){
+    //画面を一番下までスクロールする。
+    $('.right-content__middle').animate({scrollTop: $('.right-content__middle')[0].scrollHeight}, 'fast');
+  }
+
   $('#new_message').on('submit', function(e) {
-    console.log("ちゃんと動いている") //実装完了時に消します。
+    console.log("ちゃんと動いている") //画像投稿機能実装完了時、消します。
     e.preventDefault();
     var textField = $('.text');
     var content_input = textField.val();
@@ -27,11 +32,10 @@ $(function() {
       dataType: 'json',
     })
     .done(function(data) {
-    console.log(data);
       var html = buildHTML(data);
       $('ul.message').append(html);
-      $('.right-content__middle').animate({scrollTop: $('.right-content__middle')[0].scrollHeight}, 'fast'); //一番下までスクロールさせる。
       textField.val('');
+      scroll_chatview();
     })
     .fail(function() {
       alert('error');
