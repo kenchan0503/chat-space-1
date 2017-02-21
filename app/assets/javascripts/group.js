b1edce6a654e4db5771d4fe42b1bceec8340af40
shimizushinichi <q_shinichii@yahoo.co.jp>
2017-02-21 15:51:06 +0900

    グループ新規作成におけるユーザー追加機能の実装

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -1,44 +1,49 @@
-$(document).on('turbolinks:load', function() {
+$(function() {
   var preWord = ""
 
-  function appendList(user) {
-    var item = '<li class = "chat-group-user clearfix">'
-      + '<p class = "chat-group-user__name" >'     + user.nickname
-      + '<div class = "chat-group-user__btn", user_id = ' + user.id + ', user_nickname = ' + user.nickname + ' >'
+  //インクリメントサーチの結果を表示する処理
+  function appendList(user_id, user_nickname) {
+    var item = '<li class = "chat-group-user clearfix", user_id = ' + user_id + ', user_nickname = ' + user_nickname + ' >'
+      + '<p class = "chat-group-user__name" >'     + user_nickname
+      + '<div class = "chat-group-user__btn">'
       + '<p class = "chat-group-user__btn--add" >' + "追加";
     $("#user-search-result").append(item);
   }
 
-  //追加ボタンをクリックした時の処理
-  // TODO クラス名がchat-group-userのままだとkeyupのたびに消えてしまうのでクラス名を変える
-  $(document).on("click", ".chat-group-user__btn--add", function() {
-    var item = '<li class = "chat-group-user clearfix">'
-    + '<p class = "chat-group-user__name" >' + $(".chat-group-user__btn").attr("user_nickname")
-    + '<div class = "chat-group-user__btn", user_id = ' + $(".chat-group-user__btn").attr("user_id") + ' >'
-    + '<input type="hidden" name="group[user_ids][]" value=' + $(".chat-group-user__btn").attr("user_id") + '>'
-    + '<p class = "chat-group-user__btn--remove" >'          + "削除";
-    $("#chat-group-users").append(item);
+  //「追加」ボタンをクリックした時の処理
+  $(document).on("click", ".chat-group-user__btn", function() {
+    var userId = $(this).parent().attr("user_id");
+    var userNickname = $(this).parent().attr("user_nickname");
+    var item = '<li class = "chat-group-member clearfix", user_id = ' + userId + ', user_nickname = ' + userNickname + ' >'
+    + '<p class = "chat-group-member__name" >' + userNickname
+    + '<div class = "chat-group-member__btn">'
+    + '<input type="hidden" name="group[user_ids][]" value=' + userId + '>'
+    + '<p class = "chat-group-member__btn--remove" >' + "削除";
+    $("#chat-group-members").append(item);
+    $(this).parent().remove();
   });
 
-  function editInput(input){
-    var editedInput = "^" + input;
-    return editedInput;
-  }
+    //「削除」ボタンをクリックした時の処理
+  $(document).on("click", ".chat-group-member__btn", function() {
+    var userId = $(this).parent().attr("user_id");
+    var userNickname = $(this).parent().attr("user_nickname");
+    appendList(userId, userNickname);
+    $(this).parent().remove();
+  });
 
-  $(function() {
+  $(document).on('turbolinks:load', function() {
+    console.log("動いている")
     $("#user-search-field").on("keyup", function() {
-      //TODO この部分見直し
       var input          = $("#user-search-field").val();
       var splitedInputs  = input.split(" ").filter(function(e){ return e; });
-      var editedInput   = splitedInputs.map(editInput);
-      var toSearchInputs = editedInput.join("|");
+      var toSearchInputs = splitedInputs.join("|");
       var reg            = new RegExp(toSearchInputs);
 
       if (input.length != 0 && preWord != toSearchInputs) {
         $(".chat-group-user").remove();
         $.each(users, function(i, user) {
           if (user.nickname.match(reg)) {
-            appendList(user)
+            appendList(user.id, user.nickname)
           }
         })
       }
