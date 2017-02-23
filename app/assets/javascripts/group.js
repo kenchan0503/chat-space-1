3cd42a253b3d671be2cb5169770e762daa88567f
shimizushinichi <q_shinichii@yahoo.co.jp>
2017-02-23 13:50:14 +0900

    カスタムデータ属性の導入
    結局6,17行中の余計なカンマ外せばOKでした。

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -3,7 +3,7 @@ $(function() {
 
   //インクリメントサーチの結果を表示する処理
   function appendList(user_id, user_nickname) {
-    var item = '<li class = "chat-group-user clearfix", user_id = ' + user_id + ', user_nickname = ' + user_nickname + ' >'
+    var item = '<li class = "chat-group-user clearfix", data-user_id = ' + user_id + ' data-user_nickname = ' + user_nickname + ' >'
       + '<p class = "chat-group-user__name" >'     + user_nickname
       + '<div class = "chat-group-user__btn">'
       + '<p class = "chat-group-user__btn--add" >' + "追加";
@@ -12,9 +12,9 @@ $(function() {
 
   //「追加」ボタンをクリックした時の処理
   $(document).on("click", ".chat-group-user__btn", function() {
-    var userId = $(this).parent().attr("user_id");
-    var userNickname = $(this).parent().attr("user_nickname");
-    var item = '<li class = "chat-group-member clearfix", user_id = ' + userId + ', user_nickname = ' + userNickname + ' >'
+    var userId = $(this).parent().data("user_id");
+    var userNickname = $(this).parent().data("user_nickname");
+    var item = '<li class = "chat-group-member clearfix", data-user_id = ' + userId + ' data-user_nickname = ' + userNickname + ' >'
     + '<p class = "chat-group-member__name" >' + userNickname
     + '<div class = "chat-group-member__btn">'
     + '<input type="hidden" name="group[user_ids][]" value=' + userId + '>'
@@ -25,35 +25,28 @@ $(function() {
 
     //「削除」ボタンをクリックした時の処理
   $(document).on("click", ".chat-group-member__btn", function() {
-    var userId = $(this).parent().attr("user_id");
-    var userNickname = $(this).parent().attr("user_nickname");
+    var userId = $(this).parent().data("user_id");
+    var userNickname = $(this).parent().data("user_nickname");
     appendList(userId, userNickname);
     $(this).parent().remove();
   });
 
-  //既に選択されているメンバーかどうか判別する処理
+    //既に選択されているメンバーかどうか判別する処理
   function discriminant(user_id){
     elements = document.getElementsByClassName("chat-group-member");
     $.each(elements, function(i, element) {
-      existingUserId = element.getAttribute("user_id");
-      console.log("existingUserIdは⬇︎")
-      console.log(existingUserId)
-      console.log("user_idは⬇︎")
-      console.log(user_id)
-      if (user_id == 1) { //あとはここの問題さえ解決すればいける。
-        console.log("合致しました");
+      existingUserId = element.getAttribute("data-user_id");
+      if (user_id == existingUserId) {
         returnValue = "existing";
         return false;
       } else {
-        console.log("合致していません")
-        returnValue = "OK"
+        returnValue = "OK";
       }
     })
     return returnValue;
   }
 
   $(document).on('turbolinks:load', function() {
-    console.log("動いている")
     $("#user-search-field").on("keyup", function() {
       var input          = $("#user-search-field").val();
       var splitedInputs  = input.split(" ").filter(function(e){ return e; });
@@ -65,12 +58,8 @@ $(function() {
         $.each(users, function(i, user) {
           if (user.nickname.match(reg)) {
             if (discriminant(user.id) == "existing") {
-            console.log("かぶってるよ")
-            // console.log(discriminant(user.id))
+              //既存ユーザーだった場合は何も行わない。
             } else {
-            console.log("かぶってないよ︎")
-            // console.log("discriminantの結果⬇︎")
-            // console.log(discriminant(user.id))
               appendList(user.id, user.nickname)
             }
           }
