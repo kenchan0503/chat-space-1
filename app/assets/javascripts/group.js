63f556a01e6cbfe316d431f9f9cf2d023d224a73
shimizushinichi <q_shinichii@yahoo.co.jp>
2017-02-23 14:29:37 +0900

    カスタムデータ属性の使用を取りやめ
    data-user_id➡︎data-user-idになっちゃうなど、自動で名前が変更されて編集時に不具合が発生するため。

--- app/assets/javascripts/group.js
+++ app/assets/javascripts/group.js
@@ -3,8 +3,8 @@ $(function() {
 
   //インクリメントサーチの結果を表示する処理
   function appendList(user_id, user_nickname) {
-    var item = '<li class = "chat-group-user clearfix", data-user_id = ' + user_id + ' data-user_nickname = ' + user_nickname + ' >'
-      + '<p class = "chat-group-user__name" >'     + user_nickname
+    var item = '<li class = "chat-group-user clearfix", user_id = ' + user_id + ' user_nickname = ' + user_nickname + ' >'
+      + '<p class = "chat-group-user__name" >' + user_nickname
       + '<div class = "chat-group-user__btn">'
       + '<p class = "chat-group-user__btn--add" >' + "追加";
     $("#user-search-result").append(item);
@@ -12,9 +12,9 @@ $(function() {
 
   //「追加」ボタンをクリックした時の処理
   $(document).on("click", ".chat-group-user__btn", function() {
-    var userId = $(this).parent().data("user_id");
-    var userNickname = $(this).parent().data("user_nickname");
-    var item = '<li class = "chat-group-member clearfix", data-user_id = ' + userId + ' data-user_nickname = ' + userNickname + ' >'
+    var userId = $(this).parent().attr("user_id");
+    var userNickname = $(this).parent().attr("user_nickname");
+    var item = '<li class = "chat-group-member clearfix", user_id = ' + userId + ' user_nickname = ' + userNickname + ' >'
     + '<p class = "chat-group-member__name" >' + userNickname
     + '<div class = "chat-group-member__btn">'
     + '<input type="hidden" name="group[user_ids][]" value=' + userId + '>'
@@ -25,8 +25,8 @@ $(function() {
 
     //「削除」ボタンをクリックした時の処理
   $(document).on("click", ".chat-group-member__btn", function() {
-    var userId = $(this).parent().data("user_id");
-    var userNickname = $(this).parent().data("user_nickname");
+    var userId = $(this).parent().attr("user_id");
+    var userNickname = $(this).parent().attr("user_nickname");
     appendList(userId, userNickname);
     $(this).parent().remove();
   });
@@ -35,7 +35,7 @@ $(function() {
   function discriminant(user_id){
     elements = document.getElementsByClassName("chat-group-member");
     $.each(elements, function(i, element) {
-      existingUserId = element.getAttribute("data-user_id");
+      existingUserId = element.getAttribute("user_id");
       if (user_id == existingUserId) {
         returnValue = "existing";
         return false;
